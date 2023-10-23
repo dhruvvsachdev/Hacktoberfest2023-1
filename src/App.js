3d3eee9a9bf28f6191c42ee726493feb54dd7dae
dhruvvsachdev <dhruvsachdev1604@gmail.com>
2023-10-24 01:29:55 +0530

    React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Home from './Home.js';
+
+function App() {
+  return(
+    <div className="App">
+    <div>
+      <Home></Home>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import './Navbar.css';
+const Navbar = () => {
+return (
+    <>
+    <div class="navbar">
+        <a href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+</div>
+
+<div style="padding:20px">
+    <h2>Simple Navbar Example</h2>
+    <p>Scroll this page to see the effect.</p>
+    <p>Some text..</p>
+</div>
+    </>
+);
+}
+
+export default Navbar;
